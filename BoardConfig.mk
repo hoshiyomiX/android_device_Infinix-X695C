cf5c8b322e75b9d8d7cb673f11c46b9e6b83924d
Hoshiyomi <hoshiyomiX@users.noreply.github.com>
2026-07-10 14:10:59 +0000

    X695C: Add 7 device-specific configs moved from common tree
    Add device-specific configurations that were moved from
    BoardConfigCommon.mk (common-mt6785 tree) to device BoardConfig.mk.
    
    Added items:
    1. BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432 (32MB)
    2. BOARD_SUPER_PARTITION_SIZE := 8663334912 (~8.1GB)
    3. BOARD_MAIN_SIZE := 8659103744
    4. TW_DEFAULT_BRIGHTNESS := 1200
    5. TW_MAX_BRIGHTNESS := 2460
    6. TARGET_SCREEN_HEIGHT := 2460
    7. TW_STATUS_ICONS_ALIGN := center + CPU/CLOCK/BATTERY positions
    
    These are X695C-specific values that should not be inherited by
    other MT6785 devices using the same common tree.
    
    Signed-off-by: Hoshiyomi <hoshiyomiX@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,3 +16,19 @@ TARGET_OTA_ASSERT_DEVICE := X695C,Infinix-X695C
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_Infinix-X695C
 TARGET_RECOVERY_DEVICE_MODULES := libinit_Infinix-X695C
+
+# Device-specific partition sizes (moved from common BoardConfigCommon.mk)
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_SUPER_PARTITION_SIZE := 8663334912
+BOARD_MAIN_SIZE := 8659103744
+
+# Device-specific display config (moved from common BoardConfigCommon.mk)
+TW_DEFAULT_BRIGHTNESS := 1200
+TW_MAX_BRIGHTNESS := 2460
+TARGET_SCREEN_HEIGHT := 2460
+
+# StatusBar (moved from common BoardConfigCommon.mk)
+TW_STATUS_ICONS_ALIGN := center
+TW_CUSTOM_CPU_POS := "245"
+TW_CUSTOM_CLOCK_POS := "70"
+TW_CUSTOM_BATTERY_POS := "790"
