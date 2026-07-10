1909cc65d7e7519c0fca99c4b31baa1c2a8f01fc
Hoshiyomi <hoshiyomiX@users.noreply.github.com>
2026-07-10 04:36:49 +0000

    X695C: Change variant from XOS to Crimson Deviant
    Update FOX_VARIANT in vendorsetup.sh and release body in fox_common.yml.
    This reflects the custom crimson red theme + deviant patches applied to
    the recovery image.
    
    Signed-off-by: Hoshiyomi <hoshiyomiX@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -30,7 +30,7 @@ export FOX_USE_XZ_UTILS=1
 # Build Metadata
 export FOX_BUILD_TYPE="Unofficial"
 export FOX_VERSION="R11.1"
-export FOX_VARIANT="XOS"
+export FOX_VARIANT="Crimson Deviant"
 export OF_MAINTAINER="Hoshiyomi"
 
 # AVB & Treble
